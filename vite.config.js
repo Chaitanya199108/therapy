da0797e5c605b0636cd3372de3ff4b5713cdc861
Chaitanya199108 <behaviourhealingtherapycenter@gmail.com>
2025-11-08 14:53:38 +0530

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/behavioralthreapy/', // ✅ must exactly match your repo name
+  base: '/threapy/', // ✅ must exactly match your repo name
   server: {
     port: 3000,
     open: true
