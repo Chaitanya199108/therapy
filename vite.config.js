97aa091b905ecca8613044ae437ac294e2ffe899
Chaitanya199108 <behaviourhealingtherapycenter@gmail.com>
2025-11-08 15:00:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/threapy/', // ✅ must exactly match your repo name
+  base: '/therapy/', // ✅ must exactly match your repo name
   server: {
     port: 3000,
     open: true
