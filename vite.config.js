e3796033cdf4e1926f96dfe963fad3ec30f7f52d
chandra31071974 <your-github-email@example.com>
2025-11-08 12:46:45 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Behavioraltherapy/',
+  base: '/behavioralthreapy/', // ✅ must exactly match your repo name
   server: {
     port: 3000,
     open: true
