8773e2d3ca807925c6fe9d2b8bc692663e0f1975
Chaitanya199108 <behaviourhealingtherapycenter@gmail.com>
2025-11-11 11:36:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ must exactly match your repo name
+  base: '/therapy', // ✅ must exactly match your repo name
   
   }
 )
